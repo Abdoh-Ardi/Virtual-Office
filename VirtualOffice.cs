d9600b892aba4e252472349b2f4865e5ff4fbdd6
kay1C <kay1C@users.noreply.github.com>
2021-02-07 12:27:50 +0300

    implemented mySqlDB in Virtualoffice class

--- VirtualOffice.cs
+++ VirtualOffice.cs
@@ -2,37 +2,96 @@
 using System.Collections.Generic;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using Virtual_Office;
 
 public class VirtualOffice
 {
-	private string loginUserName;
-	private string loginPassword;
-	private List<Desktop> desktopList = new List<Desktop>();
-	private string status;
-	//DB properties beign
-	public string Server { get; set; }
-	public string databaseName { get; set; }
-	public string UserName { get; set; }
-	public string Password { get; set; }
-	//DB properties end
+    private string loginUserName;
+    private string loginPassword;
+    private List<Desktop> desktopList = new List<Desktop>();
+    private string status;
+    //DB properties beign
+    public string Server { get; set; }
+    public string databaseName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    private MySqlConnection Connection { get; set; }
+    private static VirtualOffice _instance = null;
 
-	public VirtualOffice(string userName, string userPassword)
-	{
-
-		Run();
-	}
-	public List<Desktop> DisplayDesktops()
-    {
-		return new List<Desktop>();
-    }
-
-
-	private void Run()
-    {
-
-    } 
-	private void Close()
+    //DB properties end
+    public VirtualOffice()
     {
 
     }
+    public VirtualOffice(string userName, string userPassword)
+    {
+
+        Run();
+    }
+    private List<Desktop> DisplayDesktops()
+    {
+        return new List<Desktop>();
+    }
+
+
+    private void Run()
+    {
+
+    }
+    private void Close()
+    {
+
+    }
+
+    //DB methods
+    public static VirtualOffice instance()
+    {
+        if (_instance == null)
+        {
+            _instance = new VirtualOffice();
+
+        }
+        return _instance;
+    }
+
+    public bool IsConnect()
+    {
+        if (Connection == null)
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(databaseName))
+                {
+                    return false;
+                }
+                string connectionString = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, databaseName, UserName, Password);
+                Connection = new MySqlConnection(connectionString);
+                Connection.Open();
+            }
+
+        }
+        return true;
+    }
+
+/**example implementation
+*var dbCon = DBConnection.Instance();
+*dbCon.Server = "YourServer";
+*dbCon.DatabaseName = "YourDatabase";
+*dbCon.UserName = "YourUsername";
+*dbCon.Password = "YourPassword";
+*if (dbCon.IsConnect())
+*{
+*    //suppose col0 and col1 are defined as VARCHAR in the DB
+*   string query = "SELECT col0,col1 FROM YourTable";
+*   var cmd = new MySqlCommand(query, dbCon.Connection);
+*   var reader = cmd.ExecuteReader();
+*    while(reader.Read())
+*    {
+*        string someStringFromColumnZero = reader.GetString(0);
+*        string someStringFromColumnOne = reader.GetString(1);
+*        Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+*    }
+*    dbCon.Close();
+*}
+	 */
 }
