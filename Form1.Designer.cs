10acf8c310d29b1fe8930dbe33d4fbcb4e117bc5
abdu515 <67963104+abdu515@users.noreply.github.com>
2021-02-07 14:37:02 +0300

    new log form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,11 @@ namespace Virtual_Office
         private void InitializeComponent()
         {
             this.Log = new System.Windows.Forms.Button();
-            this.Name = new System.Windows.Forms.TextBox();
             this.Password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Log
@@ -46,14 +47,6 @@ namespace Virtual_Office
             this.Log.Text = "Login";
             this.Log.UseVisualStyleBackColor = true;
             this.Log.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Name
-            // 
-            this.Name.Location = new System.Drawing.Point(144, 121);
-            this.Name.Name = "Name";
-            this.Name.Size = new System.Drawing.Size(112, 20);
-            this.Name.TabIndex = 1;
-            this.Name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Password
             // 
@@ -85,15 +78,34 @@ namespace Virtual_Office
             this.label2.Text = "Password";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button1.Location = new System.Drawing.Point(323, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "E&XIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 424);
+            this.ClientSize = new System.Drawing.Size(771, 424);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Password);
-            this.Controls.Add(this.Name);
             this.Controls.Add(this.Log);
             this.Name = "Form1";
             this.Text = "VirtualOffice";
@@ -106,10 +118,11 @@ namespace Virtual_Office
         #endregion
 
         private System.Windows.Forms.Button Log;
-        private System.Windows.Forms.TextBox Name;
         private System.Windows.Forms.TextBox Password;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,9 @@ namespace Virtual_Office
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            MainForm log = new MainForm();
+            log.Show();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -45,6 +47,11 @@ namespace Virtual_Office
         private void Password_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
