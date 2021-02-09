02c3120a69da9989d8b04903323fe02a11451672
kay1C <kay1C@users.noreply.github.com>
2021-02-09 06:50:37 +0300

    fixed virtual office database connection

--- VirtualOfficeUnitTest/VirtualOfficeDB.cs
+++ VirtualOfficeUnitTest/VirtualOfficeDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
 using Virtual_Office;
 
 namespace VirtualOfficeUnitTest
@@ -12,9 +13,25 @@ namespace VirtualOfficeUnitTest
         [TestMethod]
         public void connect()
         {
-            VirtualOffice vo = new VirtualOffice("localhost:3306", "mydb", "admin", "123456"); 
-            //fileNotFound errors 
+            VirtualOffice vo = new VirtualOffice("localhost", "mydb", "admin", "123456");
+            vo.Close();
 
+            //VirtualOffice vo = VirtualOffice.instance();
+            //vo.Server = "localhost";
+            //vo.databaseName = "mydb";
+            //vo.UserName = "admin";
+            //vo.Password = "123456";
+
+            //suppose col0 and col1 are defined as VARCHAR in the DB
+            //string query = "SELECT col0,col1 FROM YourTable";
+            //var cmd = new MySqlCommand(query, dbCon.Connection);
+            //var reader = cmd.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //  string someStringFromColumnZero = reader.GetString(0);
+            //string someStringFromColumnOne = reader.GetString(1);
+            //Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+            //}
         }
     }
 }
