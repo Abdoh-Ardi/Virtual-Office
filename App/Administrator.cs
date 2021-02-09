36ab20c28238fe1187d1c8e9d28a9dfa9a48b82c
abdu515 <67963104+abdu515@users.noreply.github.com>
2021-02-09 21:44:14 +0300

    update admin class -0

--- App/Administrator.cs
+++ App/Administrator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Data
 
 namespace Virtual_Office
 {
@@ -15,7 +16,29 @@ namespace Virtual_Office
             //This is my connection string i have assigned the database file address path  
             string MyConnection2 = "datasource=localhost;port=3306;username=admin;password=12345a";
             //This is my insert query in which i am taking input from the user through windows forms  
-            string Query = "insert into mydb.desktop(ip_address,desktop_name,desktop_password) values('" + desktopAddress + "','" + desktopName + "','" + loginPassword + "');";
+            string Query = "insert into mydb.desktop(ip_address,desktop_name,desktop_login_name,desktop_password) values('" + desktopAddress + "','" + desktopName + "','"+ loginName + "','" + loginPassword + "');";
+            //This is  MySqlConnection here i have created the object and pass my connection string.  
+            MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+            //This is command class which will handle the query and connection object.  
+            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+            MySqlDataReader MyReader2;
+            MyConn2.Open();
+            MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.  
+            while (MyReader2.Read())
+            {
+
+            }
+            MyConn2.Close();
+
+        }
+
+        public void AddDesktop(string loginName, string loginPassword, string desktopAddress)
+        {
+            DateTime now = DateTime.Now;
+            //This is my connection string i have assigned the database file address path  
+            string MyConnection2 = "datasource=localhost;port=3306;username=admin;password=12345a";
+            //This is my insert query in which i am taking input from the user through windows forms  
+            string Query = "insert into mydb.desktop(ip_address,desktop_name,desktop_login_name,desktop_password) values('" + desktopAddress + "','" + now + "','" + loginName + "','" + loginPassword + "');";
             //This is  MySqlConnection here i have created the object and pass my connection string.  
             MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
             //This is command class which will handle the query and connection object.  
@@ -46,12 +69,12 @@ namespace Virtual_Office
             MyConn2.Close();
 
         }
-        public void Adduser(String userName, String userpassword)
+        public void Adduser(String userName, String userpassword, String first_name, String last_name, String ip_address, String admin, String desktop_ip_address)
         {
             //This is my connection string i have assigned the database file address path  
             string MyConnection2 = "datasource=localhost;port=3306;username=admin;password=12345a";
             //This is my insert query in which i am taking input from the user through windows forms  
-            string Query = "insert into mydb.user(User_name,Password) values('" + userName + "','" + userpassword + "');";
+            string Query = "insert into mydb.user(User_name,Password,first_name,last_name,IP_Address,admin,Desktop_ip_address) values('" + userName + "','" + userpassword+ "','" + first_name + "','"+ last_name + "','"+ ip_address + "','" + admin + "','" + desktop_ip_address + "');";
             //This is  MySqlConnection here i have created the object and pass my connection string.  
             MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
             //This is command class which will handle the query and connection object.  
@@ -67,7 +90,7 @@ namespace Virtual_Office
         }
 
 
-        public void Removeuser(String userName, String userpassword)
+        public void Removeuser(String userName)
         {
             string MyConnection2 = "datasource=localhost;port=3306;username=admin;password=12345a";
             string Query = "delete from mydb.user where User_name='" + userName + "';";
@@ -84,7 +107,18 @@ namespace Virtual_Office
 
         public void DisplayActivityLog() //not completed 
         {
-            Console.WriteLine("ActivityLog");
+            string MyConnection2 = "datasource=localhost;port=3306;username=admin;password=12345a";
+            //Display query  
+            string Query = "select * from student.studentinfo;";
+            MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+            //  MyConn2.Open();  
+            MySqlDataReader rdr = MyCommand2.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(rdr);
+
+            // MyConn2.Close();
+            Console.WriteLine("no ActivityLog found");
         }
 
     }
